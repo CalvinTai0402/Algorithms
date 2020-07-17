cdf6ef519c99b8672e87e6cef5b6ebce480452c2
Tai <calvin.tai@hpe.com>
2020-07-16 21:19:52 -0600

    update .\Arrays\ .\BinaryTrees\ .\DSSpaceTimeComplexityNotes

---
+++ BinaryTrees/branchSum.py
@@ -0,0 +1,22 @@
+# Time: O(N), Space: O(N)
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branchSums(root):
+	branchSums=[]
+	getSums(root,0,branchSums)
+	return branchSums
+	
+def getSums(node,sumSoFar,branchSums):
+	if node is None:
+		return
+	sumSoFar = sumSoFar + node.value
+	if node.left is None and node.right is None:
+		branchSums.append(sumSoFar)
+		return
+	getSums(node.left,sumSoFar,branchSums)
+	getSums(node.right,sumSoFar,branchSums)
