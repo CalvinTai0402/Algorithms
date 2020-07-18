109474f6fb35d981f49915bb19b2c98aba661d36
Tai <calvin.tai@hpe.com>
2020-07-17 23:47:23 -0600

    fix tabs and spaces

--- Arrays/smallestDifference.py
+++ Arrays/smallestDifference.py
@@ -1,32 +1,33 @@
 # O(NM) T O(1) S
 def smallestDifference(arrayOne, arrayTwo):
-	minDif = float("inf")
-	for i in range(len(arrayOne)):
-		for j in range(len(arrayTwo)):
-			if abs(arrayOne[i]-arrayTwo[j]) < minDif:
-				minDif = abs(arrayOne[i]-arrayTwo[j])
-				RA = [arrayOne[i],arrayTwo[j]]
+    minDif = float("inf")
+    for i in range(len(arrayOne)):
+        for j in range(len(arrayTwo)):
+            if abs(arrayOne[i] - arrayTwo[j]) < minDif:
+                minDif = abs(arrayOne[i] - arrayTwo[j])
+                RA = [arrayOne[i], arrayTwo[j]]
     return RA
 
-#==============================AlgoExpert's answer====================================
+
+# ==============================AlgoExpert's answer====================================
 # O(NlogN + MlogM) T O(1) S
-def smallestDifference(arrayOne, arrayTwo):
+def smallestDifference2(arrayOne, arrayTwo):
     arrayOne.sort()
-	arrayTwo.sort()
-	i = 0
-	j = 0
-	smallest = float("inf")
-	while i < len(arrayOne) and j < len(arrayTwo):
-		firstNum = arrayOne[i]
-		secondNum = arrayTwo[j]
-		current = abs(arrayOne[i] - arrayTwo[j])
-		if arrayOne[i] < arrayTwo[j]:
-			i+=1
-		elif arrayOne[i] > arrayTwo[j]:
-			j+=1
-		else:	# Two numbers are the same, i.e., closest possible so return straight away
-			return [arrayOne[i],arrayTwo[j]]
-		if smallest > current:
-			smallest = current
-			smallestPair = [firstNum,secondNum]
-	return smallestPair
+    arrayTwo.sort()
+    i = 0
+    j = 0
+    smallest = float("inf")
+    while i < len(arrayOne) and j < len(arrayTwo):
+        firstNum = arrayOne[i]
+        secondNum = arrayTwo[j]
+        current = abs(arrayOne[i] - arrayTwo[j])
+        if arrayOne[i] < arrayTwo[j]:
+            i += 1
+        elif arrayOne[i] > arrayTwo[j]:
+            j += 1
+        else:  # Two numbers are the same, i.e., closest possible so return straight away
+            return [arrayOne[i], arrayTwo[j]]
+        if smallest > current:
+            smallest = current
+            smallestPair = [firstNum, secondNum]
+    return smallestPair
--- Arrays/twoNumberSum.py
+++ Arrays/twoNumberSum.py
@@ -1,13 +1,13 @@
 # O(NlogN) T O(1) S
 def twoNumberSum(array, targetSum):
-	array.sort() # O(NlogN) T, hopefully
-    left = 0 # left pointer
-	right = len(array)-1 # right pointer
-	while left < right: # Loop has O(N) T
-		if array[left]+array[right] == targetSum:
-			return [array[left],array[right]]
-		elif array[left]+array[right] < targetSum:
-			left+=1
-		elif array[left]+array[right] > targetSum:
-			right-=1
+    array.sort()  # O(NlogN) T, hopefully
+    left = 0  # left pointer
+    right = len(array) - 1  # right pointer
+    while left < right:  # Loop has O(N) T
+        if array[left] + array[right] == targetSum:
+            return [array[left], array[right]]
+        elif array[left] + array[right] < targetSum:
+            left += 1
+        elif array[left] + array[right] > targetSum:
+            right -= 1
     return []
