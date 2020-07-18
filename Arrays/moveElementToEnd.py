9c0a4792f0bd50dbd0281beac903d4490c7483f3
Tai <calvin.tai@hpe.com>
2020-07-17 23:23:07 -0600

    update moveElementToEnd.py

--- Arrays/moveElementToEnd.py
+++ Arrays/moveElementToEnd.py
@@ -1,14 +1,14 @@
 # O(N^2) T O(1) S
 def moveElementToEnd(array, toMove):
     pointer = 0
-    while pointer<len(array): 
-		if array[pointer]==toMove:
-			for i in range(pointer+1, len(array)):
-				if array[i]!=toMove:
+    while pointer<len(array):
+	    if array[pointer]==toMove:
+		    for i in range(pointer+1, len(array)):
+			    if array[i]!=toMove:
 					# swap(array, pointer, i)
-					array[pointer],array[i] = array[i],array[pointer] #This is the easier swap
-		pointer+=1
-	return array
+				    array[pointer],array[i] = array[i],array[pointer] #This is the easier swap
+	    pointer+=1
+    return array
 
 # def swap(array, indexOne, indexTwo):
 # 	temp = array[indexOne]
@@ -17,7 +17,7 @@ def moveElementToEnd(array, toMove):
 # 	return array
 
 # O(N) T O(1) S
-def moveElementToEnd(array, toMove):
+def moveElementToEnd2(array, toMove):
     leftPointer = 0
     rightPointer = len(array)-1
     while leftPointer < rightPointer:
