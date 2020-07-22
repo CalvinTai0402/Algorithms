28125e6170e3507407fa63d8248b0953419a6836
Tai <calvin.tai@hpe.com>
2020-07-22 00:26:48 -0600

    update kadanesAlgorithm.py

---
+++ famousAlgorithms/kadanesAlgorithm.py
@@ -0,0 +1,8 @@
+# O(N) T O(1) S
+def kadanesAlgorithm(array):
+    maxEndingHere = array[0]
+    maxSoFar = array[0]
+    for i in range(1, len(array)):
+        maxEndingHere = max(maxEndingHere + array[i], array[i])
+        maxSoFar = max(maxEndingHere, maxSoFar)
+    return maxSoFar
