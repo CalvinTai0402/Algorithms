7c213a2bb0aeb508f3620efd4c4d9cc96240cd9d
Tai <calvin.tai@hpe.com>
2020-07-24 23:01:56 -0600

    update selectionSort.py

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -1,11 +1,11 @@
 # O(N^2) T O(1) S
 def selectionSort(array):
     for index in range(len(array)):
-		smallest = array[index]
-		swapIndex = index
-		for j in range(index,len(array)):
-			if array[j] < smallest:
-				smallest = array[j]
-				swapIndex = j
-		array[index],array[swapIndex] = array[swapIndex],array[index]
-	return array
+        smallest = array[index]
+        swapIndex = index
+        for j in range(index, len(array)):
+            if array[j] < smallest:
+                smallest = array[j]
+                swapIndex = j
+        array[index], array[swapIndex] = array[swapIndex], array[index]
+    return array
